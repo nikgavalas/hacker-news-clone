795cf7057edf872af75fd972fad57a4a098d55fb
Nik Gavalas <nikgavalas@gmail.com>
2024-07-24 17:11:09 -0600

    Add start button

---
+++ src/components/story/story-saved.tsx
@@ -0,0 +1,19 @@
+import clsx from 'clsx';
+
+interface StorySavedProps {
+  className?: string;
+}
+
+/**
+ * @description Button to toggle the story as saved or not.
+ */
+export function StorySaved(props: StorySavedProps) {
+  const { className } = props;
+  asdf;
+
+  return (
+    <div className={clsx('', className)}>
+      <h1>StorySaved</h1>
+    </div>
+  );
+}
